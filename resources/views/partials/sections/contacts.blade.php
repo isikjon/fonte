545f97513df4f7e9a91240f582a7d6d2c12802e5
isikjon <isikjandev@gmail.com>
2025-12-11 15:24:54 +0500

    101% 11.12.2025

--- resources/views/partials/sections/contacts.blade.php
+++ resources/views/partials/sections/contacts.blade.php
@@ -2,7 +2,7 @@
     use App\Models\PageText;
     $page = $pageKey ?? 'home';
 @endphp
-<section class="contactSectionMain">
+<section class="contactSectionMain contactSectionMainPage">
     <div class="container">
         <div class="flex-contactSectionMain">
             @if(isset($showForm) && $showForm)
@@ -28,19 +28,39 @@
             </form>
             @endif
             <div class="leftFlex-contactSectionMain">
-                <h2 class="titleAll">{{ PageText::getText($page, 'contacts_title', 'Наши контакты') }}</h2>
-                <div class="block-leftFlex-contactSectionMain">
-                    <span>{{ PageText::getText('contact', 'phone_label', 'Телефон') }}</span>
-                    <a href="tel:{{ preg_replace('/[^0-9+]/', '', PageText::getText($page, 'contacts_phone', '+79885100111')) }}">{{ PageText::getText($page, 'contacts_phone', '+7 (988) 5-100-111') }}</a>
-                    <p class="contact-note">* по московскому времени</p>
-                </div>
-                <div class="block-leftFlex-contactSectionMain">
-                    <span>{{ PageText::getText('contact', 'email_label', 'E-mail') }}</span>
-                    <a href="mailto:{{ PageText::getText($page, 'contacts_email', 'dmitrieva.rostov@gmail.com') }}">{{ PageText::getText($page, 'contacts_email', 'dmitrieva.rostov@gmail.com') }}</a>
-                </div>
-                <div class="block-leftFlex-contactSectionMain">
-                    <span>{{ PageText::getText('contact', 'address_label', 'Адрес') }}</span>
-                    <p>{{ PageText::getText($page, 'contacts_address', '344056, Россия, Ростовская область, г. Ростов-на-Дону, площадь Чкалова') }}</p>
+                <div class="contactInfoBlock">
+                    <h2 class="titleAll">{{ PageText::getText($page, 'contacts_title', 'Наши контакты') }}</h2>
+                    <div class="contactInfoItem">
+                        <div class="contactInfoIcon contactInfoIcon-time"></div>
+                        <div class="contactInfoText">
+                            <h4>{{ PageText::getText($page, 'contacts_online_title', 'МЫ НА СВЯЗИ') }}</h4>
+                            <p>{{ PageText::getText($page, 'contacts_online_time', 'пн-пт 10:00-19:00') }}</p>
+                            <p class="contact-note contact-note-light">{{ PageText::getText($page, 'contacts_online_note', '* по московскому времени') }}</p>
+                        </div>
+                    </div>
+                    <div class="contactInfoItem">
+                        <div class="contactInfoIcon contactInfoIcon-phone"></div>
+                        <div class="contactInfoText">
+                            <h4>{{ PageText::getText($page, 'contacts_phone_title', 'ПОЗВОНИТЬ НАМ') }}</h4>
+                            <a href="tel:{{ preg_replace('/[^0-9+]/', '', PageText::getText($page, 'contacts_phone', '+79885100111')) }}">{{ PageText::getText($page, 'contacts_phone', '+7 (988) 5-100-111') }}</a>
+                            <p class="contact-note contact-note-light">{{ PageText::getText($page, 'contacts_phone_note', '* по московскому времени') }}</p>
+                        </div>
+                    </div>
+                    <div class="contactInfoItem">
+                        <div class="contactInfoIcon contactInfoIcon-email"></div>
+                        <div class="contactInfoText">
+                            <h4>{{ PageText::getText($page, 'contacts_email_title', 'НАПИСАТЬ НАМ') }}</h4>
+                            <a href="mailto:{{ PageText::getText($page, 'contacts_email', 'dmitrieva.rostov@gmail.com') }}">{{ PageText::getText($page, 'contacts_email', 'dmitrieva.rostov@gmail.com') }}</a>
+                        </div>
+                    </div>
+                    <div class="contactInfoItem">
+                        <div class="contactInfoIcon contactInfoIcon-company"></div>
+                        <div class="contactInfoText">
+                            <h4>{{ PageText::getText($page, 'contacts_company_title', 'Fontedijoy') }}</h4>
+                            <p>{{ PageText::getText($page, 'contacts_address_line1', '344056, Россия, Ростовская область,') }}</p>
+                            <p>{{ PageText::getText($page, 'contacts_address_line2', 'г. Ростов-на-Дону, площадь Чкалова') }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="map" id="map"></div>
